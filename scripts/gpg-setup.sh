26d844c9fef2b6f9e789bf7f90523ad60213cd95
Skyler Lemay <skyler.lemay@hingehealth.com>
2025-04-14 08:07:34 -0700

    fix scripts and paths

--- scripts/gpg-setup.sh
+++ scripts/gpg-setup.sh
@@ -5,19 +5,19 @@ gpg -k
 
 # Add additional plist files
 echo "Adding GPG launch agents"
-cp ../gpg-setup/gnupg.gpg-agent.plist $HOME/Library/LaunchAgents
+cp ./gpg-setup/gnupg.gpg-agent.plist $HOME/Library/LaunchAgents
 launchctl load $HOME/Library/LaunchAgents/gnupg.gpg-agent.plist
 
-cp ../gpg-setup/gnupg.gpg-agent-symlink.plist $HOME/Library/LaunchAgents
+cp ./gpg-setup/gnupg.gpg-agent-symlink.plist $HOME/Library/LaunchAgents
 launchctl load $HOME/Library/LaunchAgents/gnupg.gpg-agent-symlink.plist
 
 echo "Restarting GPG Agent"
 gpgconf --kill gpg-agent
 
 echo "Importing developer public keys"
-gpg --import ../gpg-setup/*.asc
+gpg --import ./gpg-setup/*.asc
 
-./setup-yubikey-ssh.sh
-./set-git-signing-key.sh
+source ./scripts/setup-yubikey-ssh.sh
+source ./scripts/set-git-signing-key.sh
 
 echo "GPG setup is complete"
--- scripts/setup-yubikey-ssh.sh
+++ scripts/setup-yubikey-ssh.sh
@@ -2,23 +2,14 @@
 
 read -p "Enter the SSH host [github.com]" host
 host=${host:-github.com}
-ssh_config=$(
-cat <<EOF
+
+echo "Insert Yubikey with stored PGP keys."
+read -p "Press enter to continue" </dev/tty
+ssh-add -L | grep "cardno:" > ~/.ssh/id_rsa_yubikey.pub
+cat >> $HOME/.ssh/config << EOF
 Host $host
   IdentitiesOnly yes
   IdentityFile ~/.ssh/id_rsa_yubikey.pub
 EOF
-)
-select key_import in yes no; do
-  case $key_import in
-    yes)
-      echo "Insert Yubikey with stored PGP keys."
-      read -p "Press enter to continue" </dev/tty
-      ssh-add -L | grep "cardno:" > ~/.ssh/id_rsa_yubikey.pub
-      cat $ssh_config >> $HOME/.ssh/config
-      echo "Yubikey SSH key added. Confirm public key is stored on $host for SSH access."
-      break;;
-    no)
-      break;;
-  esac
-done
+
+echo "Yubikey SSH key added. Confirm public key is stored on $host for SSH access."
