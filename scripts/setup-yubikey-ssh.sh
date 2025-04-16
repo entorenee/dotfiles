f118e69f9e3d61bcdc6d7a905bcc62a95a61861d
Skyler Lemay <skyler.lemay@hingehealth.com>
2025-04-16 11:09:03 -0700

    refine yubikey ssh script to check for file existence

--- scripts/setup-yubikey-ssh.sh
+++ scripts/setup-yubikey-ssh.sh
@@ -2,14 +2,22 @@
 
 read -p "Enter the SSH host [github.com]" host
 host=${host:-github.com}
+yubikey_pub=$HOME/.ssh/id_rsa_yubikey.pub
 
 echo "Insert Yubikey with stored PGP keys."
 read -p "Press enter to continue" </dev/tty
-ssh-add -L | grep "cardno:" > ~/.ssh/id_rsa_yubikey.pub
+
+if [ -f $yubikey_pub ]; then
+  echo "Yubikey SSH key already exists at $yubikey_pub. Not exporting file"
+else
+  ssh-add -L | grep "cardno:" > $yubikey_pub
+  echo "Yubikey SSH key exported to $yubikey_pub"
+fi
+
 cat >> $HOME/.ssh/config << EOF
 Host $host
   IdentitiesOnly yes
-  IdentityFile ~/.ssh/id_rsa_yubikey.pub
+  IdentityFile $yubikey_pub
 EOF
 
-echo "Yubikey SSH key added. Confirm public key is stored on $host for SSH access."
+echo "SSH configuration set. Confirm public key is stored on $host for SSH access."
