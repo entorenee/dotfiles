3ab333ed213e9da0662511e7afa7b5ece5693234
Skyler Lemay <26767995+entorenee@users.noreply.github.com>
2025-07-01 23:56:04 -0700

    eliminate gpg plist files

--- scripts/gpg-setup.sh
+++ scripts/gpg-setup.sh
@@ -1,25 +1,7 @@
 #!/usr/bin/env bash
 
-agents_dir="$HOME/Library/LaunchAgents"
-
 echo "Initializing GPG. This only needs to be done once"
 gpg -k
-
-if [ ! -d "agents_dir" ]; then
-  echo "$agents_dir does not exist. Creating it."
-  mkdir -p $agents_dir
-fi
-
-# Add additional plist files
-echo "Adding GPG launch agents"
-cp ./gpg-setup/gnupg.gpg-agent.plist "$agents_dir"
-launchctl load "$agents_dir/gnupg.gpg-agent.plist"
-
-cp ./gpg-setup/gnupg.gpg-agent-symlink.plist "$agents_dir"
-launchctl load "$agents_dir/gnupg.gpg-agent-symlink.plist"
-
-echo "Restarting GPG Agent"
-gpgconf --kill gpg-agent
 
 echo "Importing developer public keys"
 gpg --import ./gpg-setup/*.asc
