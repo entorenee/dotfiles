d5761d57e383db1f6783c78c122e13ad0831c5f3
Skyler Lemay <26767995+entorenee@users.noreply.github.com>
2025-07-09 13:18:13 -0700

    start cleaning up the old configuration

--- nix/module/nvm/default.nix
+++ nix/module/nvm/default.nix
@@ -3,6 +3,10 @@
   homebrew.brews = [
     "nvm"
   ];
+  # TODO: previously my setup script made sure that LTS was installed
+  # Determine if this will get ported over to the Nix setup
+  # nvm install --lts
+  # nvm alias default 'lts/*'
 
   home-manager.users."skyler.lemay" = { ... }: {
     xdg.configFile."nvm".source = ./config;
