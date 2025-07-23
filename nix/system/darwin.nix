e5b53732ab418da917d8fce390b3af1f2fc3194c
Skyler Lemay <26767995+entorenee@users.noreply.github.com>
2025-07-22 23:47:43 -0700

    initialize Mac configuration via Nix-Darwin

--- nix/system/darwin.nix
+++ nix/system/darwin.nix
@@ -37,6 +37,75 @@ darwin.lib.darwinSystem {
       {
         system = {
           primaryUser = username;
+          defaults = {
+            controlcenter = {
+              BatteryShowPercentage = true;
+              Bluetooth = true;
+            };
+            CustomUserPreferences = {
+              "com.apple.trackpad" = {
+                scaling = 2;
+              };
+            };
+            dock = {
+              autohide = true;
+              orientation = "left";
+              # TODO: come back to setting this up.
+              # persistent-apps = [
+              # ];
+              showhidden = true;
+              tilesize = 40;
+              wvous-tr-corner = 4;
+              wvous-br-corner = 2;
+            };
+            finder = {
+              AppleShowAllExtensions = true;
+              AppleShowAllFiles = true;
+              FXDefaultSearchScope = "SCcf"; # Default Search to current folder
+              FXEnableExtensionChangeWarning = false;
+              FXPreferredViewStyle = "Nlsv"; # Default to List View
+              FXRemoveOldTrashItems = true;
+              NewWindowTarget = "Documents";
+              ShowExternalHardDrivesOnDesktop = false;
+              ShowPathbar = true;
+            };
+            menuExtraClock = {
+              Show24Hour = true;
+            };
+            NSGlobalDomain = {
+              AppleInterfaceStyle = "Dark";
+              AppleMeasurementUnits = "Centimeters";
+              AppleMetricUnits = 1;
+              AppleShowAllExtensions = true;
+              AppleShowAllFiles = true;
+              AppleShowScrollBars = "Always";
+              AppleTemperatureUnit = "Celsius";
+              InitialKeyRepeat = 10;
+              KeyRepeat = 2;
+              NSAutomaticDashSubstitutionEnabled = false;
+              NSAutomaticPeriodSubstitutionEnabled = false;
+              NSAutomaticQuoteSubstitutionEnabled = false;
+              NSAutomaticSpellingCorrectionEnabled = false;
+              NSDocumentSaveNewDocumentsToCloud = false;
+              _HIHideMenuBar = true;
+            };
+            screensaver = {
+              askForPassword = true;
+              askForPasswordDelay = 0;
+            };
+            trackpad = {
+              Clicking = true;
+              Dragging = true;
+              TrackpadThreeFingerTapGesture = 0;
+            };
+          };
+          keyboard = {
+            enableKeyMapping = true;
+            remapCapsLockToEscape = true;
+          };
+        };
+        time = {
+          timeZone = "America/Los_Angeles";
         };
       }
     ];
