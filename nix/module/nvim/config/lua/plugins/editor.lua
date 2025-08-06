d13ec8a30fba4fd22e5485ec441069225255ec8c
Skyler Lemay <26767995+entorenee@users.noreply.github.com>
2025-08-05 22:26:08 -0700

    clean up references to copilot

--- nix/module/nvim/config/lua/plugins/editor.lua
+++ nix/module/nvim/config/lua/plugins/editor.lua
@@ -3,23 +3,6 @@ return {
   -- Commentary
   "tpope/vim-commentary",
 
-  -- GitHub Copilot
-  {
-    "github/copilot.vim",
-    event = "InsertEnter",
-  },
-
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    dependencies = {
-      "github/copilot.vim",
-      "nvim-lua/plenary.nvim",
-    },
-    config = function()
-      require('CopilotChat').setup()
-    end
-  },
-
   -- Formatting with Mason tools
   {
     "stevearc/conform.nvim",
