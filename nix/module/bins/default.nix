d22fd148b7f2dace4074d4744ab9e67df8b0ef67
Skyler Lemay <26767995+entorenee@users.noreply.github.com>
2025-08-12 16:40:15 -0700

    fix update-all bin script

--- nix/module/bins/default.nix
+++ nix/module/bins/default.nix
@@ -1,5 +1,5 @@
-{config, ...}: {
-  xdg.configFile."${config.home.homeDirectory}/.local/bin" = {
-    source = config.lib.file.mkOutOfStoreSymlink ./bin;
-  };
+{config, ...}: let
+  binPath = "${config.home.homeDirectory}/dotfiles/nix/module/bins/bin";
+in {
+  xdg.configFile."${config.home.homeDirectory}/.local/bin".source = config.lib.file.mkOutOfStoreSymlink binPath;
 }
