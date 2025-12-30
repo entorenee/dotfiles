70cd83a11f70271fda8f4ba6710dc82d2242e3b2
Skyler Lemay <26767995+entorenee@users.noreply.github.com>
2025-12-29 21:59:09 -0800

    fix: resolve treesitter config issue on Linux

--- nix/module/nvim/config/lua/plugins/ui.lua
+++ nix/module/nvim/config/lua/plugins/ui.lua
@@ -1,114 +1,112 @@
 -- UI-related plugins
 return {
-  -- Theme
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
-  -- File explorer
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    cmd = "NvimTreeToggle",
-    keys = {
-      { "<C-n>", ":NvimTreeToggle<CR>", desc = "Toggle file explorer" }
-    },
-    config = function()
-      require("nvim-tree").setup({
-        view = {
-          width = 30,
-        },
-        renderer = {
-          group_empty = true,
-        },
-        filters = {
-          dotfiles = false,
-        },
-      })
-    end,
-  },
+	-- Theme
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	-- File explorer
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		cmd = "NvimTreeToggle",
+		keys = {
+			{ "<C-n>", ":NvimTreeToggle<CR>", desc = "Toggle file explorer" },
+		},
+		config = function()
+			require("nvim-tree").setup({
+				view = {
+					width = 30,
+				},
+				renderer = {
+					group_empty = true,
+				},
+				filters = {
+					dotfiles = false,
+				},
+			})
+		end,
+	},
 
-  -- Fuzzy finder (modern alternative to CtrlP)
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-    },
-    keys = {
-      { "<C-p>", ":Telescope find_files<CR>", desc = "Find files" },
-      { "<leader>fg", ":Telescope live_grep<CR>", desc = "Live grep" },
-      { "<leader>fb", ":Telescope buffers<CR>", desc = "Find buffers" },
-    },
-    config = function()
-      require("telescope").setup({
-        defaults = {
-          file_ignore_patterns = {
-            "node_modules",
-            "coverage",
-            "dist-*",
-            ".bundle",
-            "vendor",
-            "tmp",
-            "public",
-            "themes",
-            ".git"
-          }
-        }
-      })
-    end,
-  },
+	-- Fuzzy finder (modern alternative to CtrlP)
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		keys = {
+			{ "<C-p>", ":Telescope find_files<CR>", desc = "Find files" },
+			{ "<leader>fg", ":Telescope live_grep<CR>", desc = "Live grep" },
+			{ "<leader>fb", ":Telescope buffers<CR>", desc = "Find buffers" },
+		},
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					file_ignore_patterns = {
+						"node_modules",
+						"coverage",
+						"dist-*",
+						".bundle",
+						"vendor",
+						"tmp",
+						"public",
+						"themes",
+						".git",
+					},
+				},
+			})
+		end,
+	},
 
-  -- Seamless integration between neovim and tmux panes
-  {
-    "christoomey/vim-tmux-navigator",
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-      "TmuxNavigatorProcessList",
-    },
-    keys = {
-      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-    },
-  },
+	-- Seamless integration between neovim and tmux panes
+	{
+		"christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+			"TmuxNavigatorProcessList",
+		},
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
+	},
 
-  -- TreeSitter for syntax highlighting and parsing
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "lua",
-          "nix",
-          "html",
-          "css",
-          "json",
-          "yaml",
-          "markdown",
-          "bash",
-          "sql",
-          "javascript",
-          "typescript",
-          "tsx",
-        },
-        highlight = {
-          enable = true,
-        },
-        indent = {
-          enable = true,
-        },
-      })
-    end
-  },
+	-- TreeSitter for syntax highlighting and parsing
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			ensure_installed = {
+				"lua",
+				"nix",
+				"html",
+				"css",
+				"json",
+				"yaml",
+				"markdown",
+				"bash",
+				"sql",
+				"javascript",
+				"typescript",
+				"tsx",
+			},
+			highlight = {
+				enable = true,
+			},
+			indent = {
+				enable = true,
+			},
+		},
+	},
 }
