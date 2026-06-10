e3cf6543dea2f0ba9877bde829606315442809bb
Skyler Lemay <26767995+entorenee@users.noreply.github.com>
2026-06-10 11:33:11 -0700

    refactor: update bin linking approach

--- nix/module/bins/default.nix
+++ nix/module/bins/default.nix
@@ -1,5 +1,5 @@
 {config, ...}: let
   binPath = "${config.home.homeDirectory}/dotfiles/nix/module/bins/bin";
 in {
-  xdg.configFile."${config.home.homeDirectory}/.local/bin".source = config.lib.file.mkOutOfStoreSymlink binPath;
+  home.file.".local/bin".source = config.lib.file.mkOutOfStoreSymlink binPath;
 }
